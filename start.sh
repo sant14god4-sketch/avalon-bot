70817b29ac04a645e9f98ba3960b799defba9952
sant14god4-sketch <sant14god4@gmail.com>
2026-05-30 08:41:26 -0600

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "📦 Instalando dependencias forzadamente..."
-pip install alpaca-py twelvedata flask gunicorn requests python-dotenv schedule pandas yfinance
-
-echo "🚀 Iniciando Gunicorn (pasarela de pagos)..."
-gunicorn transak_gateway:app --bind 0.0.0.0:$PORT &
-
-echo "🤖 Iniciando Bot de trading..."
-python main_bot.py
