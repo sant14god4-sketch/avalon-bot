87cdc8d95058c494966c73887727e6ee62fab780
sant14god4-sketch <sant14god4@gmail.com>
2026-05-30 00:06:56 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+echo "📦 Instalando dependencias forzadamente..."
+pip install alpaca-trade-api twelvedata flask gunicorn requests python-dotenv schedule pandas
+
+echo "🚀 Iniciando Gunicorn (pasarela de pagos)..."
 gunicorn transak_gateway:app --bind 0.0.0.0:$PORT &
+
+echo "🤖 Iniciando Bot de trading..."
 python main_bot.py
